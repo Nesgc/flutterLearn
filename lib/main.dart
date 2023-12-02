f61225fd092956d11bc1e163436270a020735f08
Nesgc <nestorgarciac11@gmail.com>
2023-12-01 19:52:23 -0600

    button center

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = 'Clickss';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-        backgroundColor: Color(0xFF2596be),
-        title: Text('App Title'),
-      )),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF2596be),
+          title: const Text('App Title'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  buttonName = 'clicked';
+                });
+
+              },
+              child: Text(buttonName)),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(color: Colors.cyan, Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
